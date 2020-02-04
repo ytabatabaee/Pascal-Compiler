6a9e1995ef1f5b1efd0a6ce3399efe05d7431bb4
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2020-02-05 01:38:00 +0330

    simple operations added

--- semantic_analyzer/CodeGenerator.java
+++ semantic_analyzer/CodeGenerator.java
@@ -14,6 +14,11 @@ public class CodeGenerator {
         semantic_stack = new Stack<>();
     }
 
+    public String resolve_type(String type1, String type2) {
+
+        return "";
+    }
+
     public void generate_code(String func) {
         Symbol res, expr1, expr2, tmp;
 
@@ -25,18 +30,44 @@ public class CodeGenerator {
                 break;
 
             case "push_real":
-                //similar to int
+                tmp = scanner.get_current();
+                tmp.setToken("float");
+                semantic_stack.push(tmp);
                 break;
 
             case "multiply":
-                expr1 = semantic_stack.pop();
                 expr2 = semantic_stack.pop();
-                res = new Symbol(expr1.getToken(), "mul " + expr1.getToken() + expr1 + "," + expr2);
+                expr1 = semantic_stack.pop();
+                res = new Symbol(expr1.getToken(), "mul " + expr1.getToken() + expr1.getVal() + "," + expr2.getVal());
                 semantic_stack.push(res);
                 break;
 
             case "add":
-                //similar to mult
+                expr2 = semantic_stack.pop();
+                expr1 = semantic_stack.pop();
+                res = new Symbol(expr1.getToken(), "add " + expr1.getToken() + expr1.getVal() + "," + expr2.getVal());
+                semantic_stack.push(res);
+                break;
+
+            case "subtract":
+                expr2 = semantic_stack.pop();
+                expr1 = semantic_stack.pop();
+                res = new Symbol(expr1.getToken(), "sub " + expr1.getToken() + expr1.getVal() + "," + expr2.getVal());
+                semantic_stack.push(res);
+                break;
+
+            case "divide":
+                expr2 = semantic_stack.pop();
+                expr1 = semantic_stack.pop();
+                res = new Symbol("float", "fdiv " + expr1.getToken() + expr1.getVal() + "," + expr2.getVal());
+                semantic_stack.push(res);
+                break;
+
+            case "mode":
+                expr2 = semantic_stack.pop();
+                expr1 = semantic_stack.pop();
+                res = new Symbol("i32", "srem " + expr1.getToken() + expr1.getVal() + "," + expr2.getVal());
+                semantic_stack.push(res);
                 break;
         }
 
